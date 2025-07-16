e91ca763f9eee1a9822f8df3bbf9ce5904a3c756
Jose Benito Badilla Barboza <104785891+jbenitobb@users.noreply.github.com>
2025-07-16 14:09:45 -0600

    Update Counter.sol
    This PR contains a security issue

--- src/Counter.sol
+++ src/Counter.sol
@@ -29,7 +29,7 @@ contract Counter {
     }
     
     function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "Counter: new owner is the zero address");
+        // require(newOwner != address(0), "Counter: new owner is the zero address");
         address oldOwner = owner;
         owner = newOwner;
         emit OwnershipTransferred(oldOwner, newOwner);
