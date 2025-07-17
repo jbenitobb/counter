fdffb552f5ddef0edffc9812b879f7d103fd1beb
Jose Benito Badilla Barboza <104785891+jbenitobb@users.noreply.github.com>
2025-07-17 14:49:24 -0600

    Update Counter-2.sol

--- src/Counter-2.sol
+++ src/Counter-2.sol
@@ -9,7 +9,6 @@ contract Counter {
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     
     modifier onlyOwner() {
-        require(msg.sender == owner, "Counter: caller is not the owner");
         _;
     }
     
