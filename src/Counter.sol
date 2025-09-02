416c94cfa89519f5dc2425fb49df09b8794adc15
Jose Benito Badilla Barboza <104785891+jbenitobb@users.noreply.github.com>
2025-09-02 16:31:06 -0600

    Update Counter.sol
    13th test

--- src/Counter.sol
+++ src/Counter.sol
@@ -9,23 +9,23 @@ contract Counter {
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     
     modifier onlyOwner() {
-        require(msg.sender == owner, "Counter: caller is not the owner");
+        // require(msg.sender == owner, "Counter: caller is not the owner");
         _;
     }
     
     constructor() {
-        owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
+        // owner = msg.sender;
+        // emit OwnershipTransferred(address(0), msg.sender);
     }
 
     function setNumber(uint256 newNumber) public onlyOwner {
-        uint256 oldNumber = number;
-        number = newNumber;
-        emit NumberSet(msg.sender, oldNumber, newNumber);
+        // uint256 oldNumber = number;
+        // number = newNumber;
+        // emit NumberSet(msg.sender, oldNumber, newNumber);
     }
 
     function increment() public {
-        number++;
+        // number++;
     }
     
     function transferOwnership(address newOwner) public onlyOwner {
