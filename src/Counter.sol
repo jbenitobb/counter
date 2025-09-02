479f8295283a9d9fa972f4f522c92c51d89d24d9
Jose Benito Badilla Barboza <104785891+jbenitobb@users.noreply.github.com>
2025-09-02 15:03:40 -0600

    Update Counter.sol
    11th test

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,28 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-contract Counter {
-    uint256 public number;
-    address public owner;
-    
-    event NumberSet(address indexed setter, uint256 oldNumber, uint256 newNumber);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Counter: caller is not the owner");
-        _;
-    }
-    
-    constructor() {
-        owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
-    }
-
-    function setNumber(uint256 newNumber) public onlyOwner {
-        uint256 oldNumber = number;
-        number = newNumber;
-        emit NumberSet(msg.sender, oldNumber, newNumber);
-    }
+  // SPDX-License-Identifier: UNLICENSED
+  pragma solidity ^0.8.13;
+  
+  contract Counter {
+      uint256 public number;
+      address public owner;
+       
+      event NumberSet(address indexed setter, uint256 oldNumber, uint256 newNumber);
+      event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+      
+      modifier onlyOwner() {
+          require(msg.sender == owner, "Counter: caller is not the owner");
+          _;
+      }
+      
+      constructor() {
+          owner = msg.sender;
+          emit OwnershipTransferred(address(0), msg.sender);
+      }
+  
+      function setNumber(uint256 newNumber) public onlyOwner {
+          uint256 oldNumber = number;
+          number = newNumber;
+          emit NumberSet(msg.sender, oldNumber, newNumber);
+      }
 
     function increment() public {
         number++;
