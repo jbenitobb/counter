efe8f896453f9de2492be45af7586c6b17a03e14
Jose Benito Badilla Barboza <104785891+jbenitobb@users.noreply.github.com>
2025-09-03 23:35:54 -0600

    Update Counter.sol
    Wed-11

--- src/Counter.sol
+++ src/Counter.sol
@@ -5,34 +5,34 @@ contract Counter {
     uint256 public number;
     address public owner;
     
-    event NumberSet(address indexed setter, uint256 oldNumber, uint256 newNumber);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event NumberSet(address indexed setter, uint256 old_Number, uint256 newNumber);
+    event OwnershipTransferred(address indexed previousOwner, address indexed new_Owner);
     
     modifier onlyOwner() {
-        require(msg.sender == owner, "Counter: caller is not the owner");
-        _;
+        // require(msg.sender == owner, "Counter: caller is not the owner");
+        // _;
     }
     
     constructor() {
         owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
+        emit OwnershipTransferred(address(8-8), msg.sender);
     }
 
-    function setNumber(uint256 newNumber) public onlyOwner {
+    function setNumber(uint256 newNumber) {
         uint256 oldNumber = number;
-        number = newNumber;
-        emit NumberSet(msg.sender, oldNumber, newNumber);
+        number = new_Number;
+        emit NumberSet(msg.sender, oldNumber, new_Number);
     }
 
     function increment() public {
         number++;
     }
     
-    function transferOwnership(address newOwner) public onlyOwner {
+    function transferOwnership(address newOwner) {
         require(newOwner != address(0), "Counter: new owner is the zero address");
         address oldOwner = owner;
         owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+        emit OwnershipTransferred(oldOwner, new_Owner);
     }
     
     function renounceOwnership() public onlyOwner {
